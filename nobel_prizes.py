9e04569f0f4597dd95826b7b5c23f6c96b3885db
Masud <masudabdulkadir@outlook.com>
2021-02-11 14:55:13 +0000

    Initial Commit

--- nobel_prizes.py
+++ nobel_prizes.py
@@ -22,7 +22,6 @@ print("The number of prizes won by the top 10 nationalities:")
 nobel['birth_country'].value_counts().head(10)
 
 
-
 # Calculating the proportion of USA born winners per decade
 nobel['usa_born_winner'] = nobel['birth_country'] == 'United States of America'
 nobel['decade'] = np.floor(nobel['year']).astype('int')
@@ -30,7 +29,6 @@ prop_usa_winners = nobel.groupby(['decade'], as_index=False)['usa_born_winner'].
 
 # Display the proportions of USA born winners per decade.
 display(prop_usa_winners)
-
 
 
 # Setting the plotting theme
@@ -47,7 +45,6 @@ from matplotlib.ticker import PercentFormatter
 ax.yaxis.set_major_formatter(PercentFormatter(1.0))
 
 
-
 # Calculating the proportion of female laureates per decade
 nobel['female_winner'] = nobel['sex'] == 'Famale'
 prop_female_winners = nobel.groupby(['decade', 'category'], as_index=False)['female_winner'].mean()
@@ -60,15 +57,12 @@ from matplotlib.ticker import PercentFormatter
 ax.yaxis.set_major_formatter(PercentFormatter(1.0))
 
 
-
 # Picking out the first woman to win a Nobel Prize
 nobel[nobel.sex == 'Female'].nsmallest(1, 'year')
 
 
-
 # Selecting the laureates that have received 2 or more prizes.
 nobel.groupby('full_name').filter(lambda win: len(win) >= 2)
-
 
 
 # Converting birth_date from String to datetime
@@ -82,11 +76,9 @@ sns.lmplot(x='year', y='age', data=nobel, lowess=True,
            aspect=2, line_kws={'color' : 'black'})
 
 
-
 # Same plot as above, but separate plots for each type of Nobel Prize
 sns.lmplot(x='year', y='age', data=nobel, row='category', lowess=True, 
            aspect=2, line_kws={'color' : 'black'})
-
 
 
 # The oldest winner of a Nobel Prize as of 2016
